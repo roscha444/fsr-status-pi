e502ea9f984bf9784357c12a542d5f4e529e8f92
Robin Schumacher <info@robin-schumacher.com>
2022-05-17 15:35:09 +0200

    log to file

--- src/runner.py
+++ src/runner.py
@@ -1,8 +1,23 @@
 import status
 import logging
 from dotenv import load_dotenv
+import sys
 
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler('logs.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stdout_handler)
+
 load_dotenv()
 logging.info('Runner started...')
 status.main()
